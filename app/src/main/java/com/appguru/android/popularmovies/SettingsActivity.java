3c26742a938ee7c00c0e58e07f9c0e305b431cc0
nilohit-github <jhanilohit9286@gmail.com>
2016-06-07 07:34:00 -0500

    SettingsActivity.java fixed as per review comment
    Code review fixed

--- app/src/main/java/com/appguru/android/popularmovies/SettingsActivity.java
+++ app/src/main/java/com/appguru/android/popularmovies/SettingsActivity.java
@@ -201,7 +201,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         public boolean onOptionsItemSelected(MenuItem item) {
             int id = item.getItemId();
             if (id == android.R.id.home) {
-                startActivity(new Intent(getActivity(), SettingsActivity.class));
+                startActivity(new Intent(getActivity(), MainActivity.class));
                 return true;
             }
             return super.onOptionsItemSelected(item);
